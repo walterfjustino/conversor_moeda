3cd178796b79daedf8e1006a205210608bc6e1d5
Walter Fernandes Justino <walterfjustino@gmail.com>
2020-04-18 23:09:21 -0300

    Third_commit - Created the interface

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,16 @@ void main() async {
   //FUNÇÃO ASSINCRONA
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData( //DEFINE UM TEMA PARA O APP INTEIRO
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )),
   ));
 }
 
@@ -29,6 +39,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,10 +76,40 @@ class _HomeState extends State<Home> {
                     textAlign: TextAlign.center,)
                   );
                 }else{//CASO NÃO OCORRA ERRO SERÁ EXECUTADO
-                  return Container(color: Colors.green,);
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
+                        buildTextField("Reais", "R\$"),
+                        Divider(),
+                        buildTextField("Dólares", "US\$"),
+                        Divider(),
+                        buildTextField("Euros", "€"),
+                    ],
+                    ),
+                  );
                 }
             }
           }),
     );
   }
+}
+
+Widget buildTextField(String label, String prefix){
+  return TextField(
+    decoration: InputDecoration(
+      labelText: "Euros",
+      labelStyle: TextStyle(color: Colors.amber),
+      border: OutlineInputBorder(),
+      prefixText: prefix,
+    ),
+    style: TextStyle(
+      color: Colors.amber, fontSize: 25.0
+    ),
+  );
+
 }
